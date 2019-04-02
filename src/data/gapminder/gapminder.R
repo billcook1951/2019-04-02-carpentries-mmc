c722fefde8e2f35411b5ea2c850352f29bd5b6f6
BillCook1951 <BillCook.PhD@gmail.com>
2019-04-02 17:06:37 +0000

    Summarize gapminder and create plot of Africa lifeExpectancy

---
+++ src/data/gapminder/gapminder.R
@@ -0,0 +1,5 @@
+# Download the Gapinder data for analysis
+#
+# Library
+
+file_url <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv"
